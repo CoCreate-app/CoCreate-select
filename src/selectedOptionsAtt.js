978eeeb96287162f18e873c23aafb782c8196cff
frank <frank@cocreate.app>
2021-05-08 22:30:15 +0000

    fix'

--- src/selectedOptionsAtt.js
+++ src/selectedOptionsAtt.js
@@ -1,7 +1,7 @@
 import {container} from './select';
 
 export default function(cond) {
-    Object.defineProperty(window.HTMLElement.prototype, 'selectedIndex', {
+    Object.defineProperty(window.HTMLElement.prototype, 'selectedOptions', {
         get: function() {
             if (cond(this) && container.has(this))
                 return container.get(this).selectedContainer.children;
