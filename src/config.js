35ca4dc9a42910fc44c0de25239c8113db8ed6ab
frank <frank@cocreate.app>
2021-05-09 00:05:42 +0000

    comment

--- src/config.js
+++ src/config.js
@@ -1,10 +1,16 @@
 // options
 export const containerSelector = 'cocreate-select, div.select--field';
+
 export const inputSelector = 'input';
-export const optionsSelector = 'cc-options';
-export const optionSelector = "cc-option";
-export const optionTagName = "cc-option";
 export const selectedTagName = "selected";
+export const optionsSelector = 'cc-options';
+
+// anything matches optionSelector considered as option even if not the same as optionTagName
+export const optionSelector = "cc-option";
+
+// new options created according this tag name
+export const optionTagName = "cc-option";
+
 export const removeMarkup = '<span class="remove">x</span>'
 
 
