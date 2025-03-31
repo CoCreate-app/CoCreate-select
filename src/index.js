ddd4518a2a955374f577af0a5cff383a16a4f4a9
frankpagan <frank@cocreate.app>
2025-03-31 06:57:39 -0600

    fix: update observer obseve param to type and and attributeName to attributeFilter

--- src/index.js
+++ src/index.js
@@ -1,49 +1,47 @@
 import CoCreateSelect from "./select.js";
-import observer from '@cocreate/observer';
-import { getAttributeNames } from '@cocreate/utils';
-import { containerSelector } from './config';
-import './index.css';
+import observer from "@cocreate/observer";
+import { getAttributeNames } from "@cocreate/utils";
+import { containerSelector } from "./config";
+import "./index.css";
 
 function init() {
-    let elements = document.querySelectorAll(containerSelector);
-    initElements(elements);
+	let elements = document.querySelectorAll(containerSelector);
+	initElements(elements);
 }
 
 function initElements(elements) {
-    for (let element of elements)
-        initElement(element);
+	for (let element of elements) initElement(element);
 }
 
 function initElement(element) {
-    CoCreateSelect.init(element);
+	CoCreateSelect.init(element);
 }
-
 
 // SelectAdapter.init();
 // CoCreateSelect.adapter = SelectAdapter;
 
 observer.init({
-    key: 'CoCreateSelectAttributes',
-    observe: ['attributes'],
-    attributeName: getAttributeNames(['array', 'object', 'key']),
-    selector: 'cocreate-select',
-    callback: function (mutation) {
-        CoCreateSelect.init(mutation.target);
-    }
+	key: "CoCreateSelectAttributes",
+	types: ["attributes"],
+	attributeFilter: getAttributeNames(["array", "object", "key"]),
+	selector: "cocreate-select",
+	callback: function (mutation) {
+		CoCreateSelect.init(mutation.target);
+	}
 });
 
 observer.init({
-    name: 'CoCreateSelect',
-    observe: ['addedNodes'],
-    selector: 'cocreate-select',
-    callback: function (mutation) {
-        CoCreateSelect.init(mutation.target);
-    }
+	name: "CoCreateSelect",
+	types: ["addedNodes"],
+	selector: "cocreate-select",
+	callback: function (mutation) {
+		CoCreateSelect.init(mutation.target);
+	}
 });
 
 // observer.init({
 // 	name: 'CoCreateSelect',
-// 	observe: ['childList'],
+// 	types: ['childList'],
 // 	target: 'cc-option',
 // 	callback: function(mutation) {
 // 		CoCreateSelect.init(mutation.target)
@@ -52,5 +50,5 @@ observer.init({
 
 init();
 
-export * from './select';
+export * from "./select";
 export default CoCreateSelect;
